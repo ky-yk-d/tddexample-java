a2bb09e32f8f4002c46723508daf4f8e9923861c
ky-yk-d <key0of7one0s5life@gmail.com>
2020-05-02 01:49:14 +0900

    test1: green

--- src/main/java/tddexample/Product.java
+++ src/main/java/tddexample/Product.java
@@ -2,10 +2,13 @@ package tddexample;
 
 public class Product {
 
+    private Object category;
+
     public void setCategory(Object aCategory) {
+        category = aCategory;
     }
 
     public Object getCategory() {
-        return null;
+        return category;
     }
 }
