79b67ab0ef56e95726787f7d4d28db5c091d257c
ky-yk-d <key0of7one0s5life@gmail.com>
2020-05-02 01:40:56 +0900

    test1: red

---
+++ src/main/java/tddexample/Product.java
@@ -0,0 +1,11 @@
+package tddexample;
+
+public class Product {
+
+    public void setCategory(Object aCategory) {
+    }
+
+    public Object getCategory() {
+        return null;
+    }
+}
---
+++ src/test/java/tddexample/Test1.java
@@ -0,0 +1,17 @@
+package tddexample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test1 {
+
+    @Test
+    public void test1() {
+        Product pc = new Product();
+        Object electronic = new Object();
+        pc.setCategory(electronic);
+        assertEquals(electronic, pc.getCategory());
+    }
+
+}
