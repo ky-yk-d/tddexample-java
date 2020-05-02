aec1600037baf8064c7268c41a7cda7ab2ff026e
ky-yk-d <key0of7one0s5life@gmail.com>
2020-05-02 16:42:39 +0900

    test1: green

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,10 +1,13 @@
 public class Product {
 
+    private Object category;
+
     public Object getCategory() {
-        return null;
+        return this.category;
     }
 
     public void setCategory(Object aCategory) {
+        this.category = aCategory;
     }
 
 }
